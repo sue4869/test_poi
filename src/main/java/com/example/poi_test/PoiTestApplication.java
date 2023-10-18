04f1d72952850efc3f3a99df59ef77d1a5ef5021
sue4869 <sue4869@naver.com>
2023-10-18 12:20:17 +0900

    init setting

---
+++ src/main/java/com/example/poi_test/PoiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.poi_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoiTestApplication.class, args);
+	}
+
+}
